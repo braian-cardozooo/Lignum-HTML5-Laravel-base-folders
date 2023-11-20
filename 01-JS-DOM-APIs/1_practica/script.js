3f2bf426a82cdde6a1d25116b9bda8606128c82e
braian-cardozooo <121458775+braian-cardozooo@users.noreply.github.com>
2023-11-20 16:10:47 -0300

    1.1

---
+++ 01-JS-DOM-APIs/1_practica/script.js
@@ -0,0 +1,9 @@
+//aqui se crea el evento del loading
+window.addEventListener("DOMContentLoaded", function() {
+    //Se obtiene la referencia a la sección con el ID "fa" del section
+    var pp = document.getElementById("fa");
+
+       // Mostrar la sección con la clase "fade-in"
+        pp.classList.add("fade-in");
+    
+});
